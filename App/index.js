f8ef48aa2decd792e96dc1559ed130eb3ef8f741
Jin Hao <limjinhao@gmail.com>
2020-05-19 01:06:35 +0800

    Added eslint and prettier to project

--- App/index.js
+++ App/index.js
@@ -1,12 +1,12 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
 
-import useCachedResources from './hooks/useCachedResources';
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import LinkingConfiguration from './navigation/LinkingConfiguration';
-import {LoginScreen} from "./screens";
+import useCachedResources from "./hooks/useCachedResources";
+import BottomTabNavigator from "./navigation/BottomTabNavigator";
+import LinkingConfiguration from "./navigation/LinkingConfiguration";
+import { LoginScreen } from "./screens";
 
 const Stack = createStackNavigator();
 
@@ -18,10 +18,10 @@ export default function App(props) {
   } else {
     return (
       <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
+        {Platform.OS === "ios" && <StatusBar barStyle="dark-content" />}
         <NavigationContainer linking={LinkingConfiguration}>
-          <Stack.Navigator initalRouteName = "Login">
-            <Stack.Screen name="Login" component = {LoginScreen} />
+          <Stack.Navigator initalRouteName="Login">
+            <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="Root" component={BottomTabNavigator} />
           </Stack.Navigator>
         </NavigationContainer>
@@ -33,6 +33,6 @@ export default function App(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
